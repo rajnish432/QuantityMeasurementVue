6b74a8ca8689831ac0aeaee8cb168232aaa77f1b
rajnish432 <rajnish.kahar1996@gmail.com>
2020-09-16 19:07:02 +0530

    [Rajnish]ADDED: Welcome To QuantityMeasurement

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <QuantityMeasurementHeader/>
+  </div>
+</template>
+
+<script>
+import QuantityMeasurementHeader from './components/QuantityMeasurementHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    QuantityMeasurementHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QuantityMeasurementHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuantityMeasurementHeader',
+  data(){
+    return{
+      message:'Welcome to QuantityMeasurement'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
